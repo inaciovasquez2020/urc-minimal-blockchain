bfec19657996bebf47db8c3a9ea488f1b96709de
Inacio F. Vasquez <inaciof.vasquez@Inacios-MacBook-Air.local>
2026-01-30 00:27:24 -0300

    Add persistent storage layer to preserve blockchain state

--- urc_storage.py
+++ urc_storage.py
@@ -1,23 +1,21 @@
-# urc_storage.py
+import pickle
+import os
+from urc_chain import Blockchain
 
-import json
-from urc_chain import Chain, genesis_block, verify_chain
+def _filename(address):
+    safe = address.replace(":", "_")
+    return f"chain_{safe}.dat"
 
-CHAIN_FILE = "chain.json"
+def load_chain(address):
+    fname = _filename(address)
+    if os.path.exists(fname):
+        with open(fname, "rb") as f:
+            return pickle.load(f)
+    else:
+        return Blockchain()
 
-def save_chain(chain: Chain):
-    with open(CHAIN_FILE, "w") as f:
-        json.dump(chain.blocks, f, indent=2)
-
-def load_chain():
-    try:
-        with open(CHAIN_FILE, "r") as f:
-            blocks = json.load(f)
-        chain = Chain(genesis_block())
-        chain.blocks = blocks
-        if not verify_chain(chain):
-            raise Exception("Invalid chain on disk")
-        return chain
-    except FileNotFoundError:
-        return Chain(genesis_block())
+def save_chain(address, chain):
+    fname = _filename(address)
+    with open(fname, "wb") as f:
+        pickle.dump(chain, f)
 
